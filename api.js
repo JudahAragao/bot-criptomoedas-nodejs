8789f52db41513ccfe338159bc2603acfc9507a8
Judah Aragão <judah.aragao@gmail.com>
2022-08-02 00:27:47 -0300

    order function

--- api.js
+++ api.js
@@ -1,12 +1,51 @@
 const axios = require('axios')
 const querystring = require('querystring')
+const crypto = require('crypto')
+
+const apiKey = process.env.API_KEY_TEST
+const apiSecret = process.env.SECRET_KEY_TEST
+const apiUrl = process.env.API_URL_TEST
+
+const privateCall = async (path, data = {}, method = 'GET') => {
+    const timestamp = Date.now()
+    const signature = crypto.createHmac('sha256', apiSecret)
+        .update(`${querystring.stringify({...data, timestamp})}`)
+        .digest('hex')
+
+    const newData = {...data, timestamp, signature}
+    const qs = `?${querystring.stringify(newData)}`
+
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        })
+        return result.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const newOrder = (symbol, quantity, price, side='BUY', type='MARKET') => {
+    const data = { symbol, side, type, quantity }
+
+    if (price) data.price = price;
+    if (type === "LIMIT") data.timeInForce = 'GTC'
+
+    return privateCall('/v3/order', data, 'POST')
+}
+
+const accountInfo = async () => {
+    return privateCall('/v3/account')
+}
 
 const publicCall = async (path, data, method = "GET") => {
     try {
         const qs = data ? `${querystring.stringify(data)}` : ''
         const result = await axios({
             method,
-            url: `${process.env.API_URL}${path}${qs}`
+            url: `${apiUrl}${path}${qs}`
         })
         return result.data
     } catch (error) {
@@ -22,4 +61,8 @@ const depth = async (symbol = 'BTCBRL', limit = 5) => {
     return publicCall('/v3/depth?', { symbol, limit })
 }
 
-module.exports = { time, depth }
+const exchangeInfo = async () => {
+    return publicCall('/v3/exchangeInfo')
+}
+
+module.exports = { time, depth, exchangeInfo, accountInfo, newOrder }
--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const api = require('./api')
+const symbol = process.env.SYMBOL
+const profitability = parseFloat(process.env.PROFITABILITY)
+const coinFiat = process.env.COIN_FIAT
+const goodBuy = process.env.GOOD_BUY
 
 setInterval(async () => {
-    const result = await api.depth()
-    console.log(`Highest Buy: ${result.bids[0][0]}`)
-    console.log(`Lowest Buy: ${result.asks[0][0]}`)
+    let buy, sell
+    const result = await api.depth(symbol)
+    
+    if (result.bids && result.bids.length) {
+        console.log(`Highest Buy: ${result.bids[0][0]}`)
+        buy = parseFloat(result.bids[0][0])
+    }
 
-    const sell = parseInt(result.asks[0][0])
-    const buy = parseInt(result.bids[0][0])
+    if (result.asks && result.asks.length) {
+        console.log(`Lowest Sell: ${result.asks[0][0]}`)
+        sell = parseFloat(result.asks[0][0])
+    }
 
     if (sell < 200000) {
         console.log('Hora de comprar')
+        
+        const account = await api.accountInfo()
+        const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1)
+
+        console.log('POSIÇÃO DA CARTEIRA')
+        console.log(coins)
+
+        console.log('Verificando Grana')
+        const walletUSD = parseFloat(coins.find(c => c.asset === coinFiat).free)
+        if (sell <= walletUSD) {
+            console.log('Temos grana! comprando agora')
+            const buyOrder = await api.newOrder(symbol, 1)
+            console.log(`orderId: ${buyOrder.orderId}`)
+            console.log(`status: ${buyOrder.status}`)
+
+            console.log('Posicionando venda futura...')
+            const price = parseFloat(sell * profitability).toFixed(8)
+            console.log(`Vendendo por ${price} (${profitability})`)
+            const sellOrder = await api.newOrder(symbol, 1, price, 'SELL', 'LIMIT')
+            console.log(`orderId: ${sellOrder.orderId}`)
+            console.log(`status: ${sellOrder.status}`)
+        }
     } else if (buy > 230000) {
         onsole.log('Hora de vender')
     } else {
